66d3d21bb52ff03956f74b0331427e4bdafa9e50
Denis Tereshkin <denis@kasan.ws>
2016-01-27 22:01:45 +0600

    Testing framework

---
+++ tests/test.cpp
@@ -0,0 +1,23 @@
+/*
+ * test.cpp
+ */
+
+#define CATCH_CONFIG_RUNNER
+
+#include "catch.hpp"
+#include "gmock/gmock.h"
+
+int main(int argc, char** argv)
+{
+	::testing::GTEST_FLAG(throw_on_failure) = true;
+	::testing::InitGoogleMock(&argc, argv);
+
+	int result = Catch::Session().run( argc, argv );
+	return result;
+}
+
+
+
+
+
+
